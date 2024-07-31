38cb67fe97fdbe3c1ea44bd7b55c2083c5ebde67
muhammadtalal8 <muhammadtalalshakeel2009@gmail.com>
2024-07-31 20:22:43 +0500

    shipping cost

--- hello_world/lib/main1.dart
+++ hello_world/lib/main1.dart
@@ -1,31 +1,24 @@
 void main() {
-   String destinationZone = 'PQR';
-     double weightinkg = 6;
-     if(destinationZone =='PQR'){
-       print('shipping cost: ${weightinkg*10}');
-     }
-     else if(destinationZone =='XYZ'){
-       print('shipping cost: ${weightinkg*5}');
-     }
-     else if(destinationZone =='ABC'){
-       print('shipping cost: ${weightinkg*7}');
-     } 
-    else{ print('invalid destination zone');
-}
-  
-  
-  
-  switch(destinationZone)
-  {
-    case 'PQR':
-   print('shipping cost: ${weightinkg*10}');
-    
-    case 'XYZ':
-   print('shipping cost: ${weightinkg*5}');
-    case 'ABC':
-   print('shipping cost: ${weightinkg*7}');
-
+  String destinationZone = 'PQR';
+  double weightinkg = 6;
+  if (destinationZone == 'PQR') {
+    print('shipping cost: ${weightinkg * 10}');
+  } else if (destinationZone == 'XYZ') {
+    print('shipping cost: ${weightinkg * 5}');
+  } else if (destinationZone == 'ABC') {
+    print('shipping cost: ${weightinkg * 7}');
+  } else {
+    print('invalid destination zone');
   }
 
-
+  switch (destinationZone) {
+    case 'PQR':
+      print('shipping cost: ${weightinkg * 10}');
+      break;
+    case 'XYZ':
+      break;
+      print('shipping cost: ${weightinkg * 5}');
+    case 'ABC':
+      print('shipping cost: ${weightinkg * 7}');
+  }
 }
