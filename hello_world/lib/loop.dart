a3da57d41bf96f10a27e84b758ca1ff7ce09be87
muhammadtalal8 <muhammadtalalshakeel2009@gmail.com>
2024-08-01 21:42:48 +0500

    loop

--- hello_world/lib/loop.dart
+++ hello_world/lib/loop.dart
@@ -1,6 +1,11 @@
 void main() {
   //for loop
-  for (int i = 0; i < 10; i++) {
+  for (int i = 0; i < 15; i++) {
     print('hello world ${i + 1}');
+  }
+  String value = 'hello1';
+  for(int i=0; i<value.length; i++)
+  {
+    print(value[i]);
   }
 }
