eaba22b66a5efeb3daeedbdaeb8a0c6c5fd49246
muhammadtalal8 <muhammadtalalshakeel2009@gmail.com>
2024-08-03 14:24:22 +0500

    oop

--- hello_world/lib/oop.dart
+++ hello_world/lib/oop.dart
@@ -3,14 +3,19 @@ void main() {
   print(car.engineworking);
   print(car.lighton);
   print(car.speed);
-  
+  print(car.wheels);
+  Vehicle truck = Truck();
+  print(truck.engineworking);
+  print(truck.lighton);
+  print(truck.speed);
+  print(truck.wheels);
 }
 
 class Vehicle {
   int speed = 10;
   bool engineworking = false;
   bool lighton = true;
-
+  int wheels = 10;
   void accelerate() {
     speed += 10;
   }
@@ -18,6 +23,13 @@ class Vehicle {
 
 class Car extends Vehicle {
   int wheels = 4;
+  void printsomething() {
+    print(wheels);
+  }
+}
+
+class Truck extends Vehicle {
+  int wheels = 8;
   void printsomething() {
     print(wheels);
   }
