c181a063598deea10a55acc11d11c92a8cf81ee2
muhammadtalal8 <muhammadtalalshakeel2009@gmail.com>
2024-08-10 16:45:55 +0500

    assignment.dart

--- hello_world/lib/assignment.dart
+++ hello_world/lib/assignment.dart
@@ -1,6 +1,6 @@
 void main(){
  //list 1
-  var myList= [1,2,3,4,5];
+  var myList= [1,2,3,4,5,6];
  print(myList.sublist(1,3));
  //list 2
  print(myList.sublist(1));
@@ -8,22 +8,22 @@ void main(){
  myList.shuffle();
  print('$myList');
  //list 4
-var ascList = [1,2,3,4,5,6];
+var ascList = [1,2,3,4,5,];
  print(ascList.reversed.toList());
  //list 5
- var mylist   = [1, 2, 3, 4, 5];
+ var mylist   = [1, 2, 3, 4, 5,6];
  print(myList.getRange(2,5));
  //list 6
-var List2= [0,1,2,3,4,5,6];
+var List2= [0,1,2,3,4,5,6,7];
  List2.replaceRange(2,3, [10]);
  //list 7
-var firstList = [1,2,3,4,5,6];
+var firstList = [1,2,3,4,5,6,7,8];
  print(firstList.firstWhere((i) => i > 3));
  
  var sList = ['one', 'two', 'three', 'four'];
  print(sList.firstWhere((i) => i.length > 2));
  //list 8
-var mList = [1, 2, 3, 4];
+var mList = [1, 2, 3, 4,5,6];
  print(mList.singleWhere((i) => i == 3));
  //list 9
  var sportsList = ['cricket', 'tennis', 'football'];
