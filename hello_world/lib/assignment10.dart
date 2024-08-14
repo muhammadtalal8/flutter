59e680f05fcf19aacea8492dde4929ffefce84f2
muhammadtalal8 <muhammadtalalshakeel2009@gmail.com>
2024-08-14 19:07:27 +0500

    first commit

--- hello_world/lib/assignment10.dart
+++ hello_world/lib/assignment10.dart
@@ -1,7 +1,21 @@
 import 'dart:io';
 
 void main() {
- int? n = int.parse(stdin.readLineSync()!);
+ 
+ 
+
+  // Loop to print numbers from 1 to 10
+  for (int i = 1; i <= 10; i++) {
+    print(i);
+  
+}
+
+ 
+ 
+ 
+ 
+ 
+  int? n = int.parse(stdin.readLineSync()!);
 
   int factorial = 1;
   int i = n;
@@ -11,46 +25,43 @@ void main() {
   }
   print('The factorial of $n is $factorial');
 
-
-  
-
   String input = "Hello, how are you?";
-  
+
   int vowelCount = 0;
-  
+
   for (int i = 0; i < input.length; i++) {
     String letter = input[i];
-    
-    if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u' ||
-        letter == 'A' || letter == 'E' || letter == 'I' || letter == 'O' || letter == 'U') {
+
+    if (letter == 'a' ||
+        letter == 'e' ||
+        letter == 'i' ||
+        letter == 'o' ||
+        letter == 'u' ||
+        letter == 'A' ||
+        letter == 'E' ||
+        letter == 'I' ||
+        letter == 'O' ||
+        letter == 'U') {
       vowelCount++;
     }
   }
-  
+
   print("Number of vowels: $vowelCount");
 
+  // Loop to print numbers from 10 to 1
+  for (int i = 10; i >= 1; i--) {
+    print(i);
+  }
 
-
-
-
-
- 
- 
- 
- 
   generatePattern(4);
 
+  List number = [2, 3, 4, 5, 6, 7, 8, 9, 1, 0];
 
-
-
-      List number = [2, 3, 4, 5, 6, 7, 8, 9, 1, 0];
-
-      for (int number in number) {
-        if (number > 5) {
-          print(number);
-        }
-      }
-
+  for (int number in number) {
+    if (number > 5) {
+      print(number);
+    }
+  }
 
 //largest number
   List<int> numbers = [22, 32, 99, 65, 46, 59];
@@ -65,25 +76,28 @@ void main() {
 
   print('The largest number is $largest');
 
-  // Generate and print the first 10 numbers in the Fibonacci sequence
   for (int i = 0; i < 10; i++) {
     print(fibonacci(i));
   }
-}
-
 int fibonacci(int n) {
-  // The first two numbers in the sequence are 0 and 1
   if (n == 0 || n == 1) {
     return n;
   }
 
-  // The nth number in the sequence is the sum of the (n - 1)th and (n - 2)th numbers
   return fibonacci(n - 1) + fibonacci(n - 2);
-
-
-
-
 }
+
+  int j = 1;
+
+  // While loop to print numbers from 1 to 3
+  while (j <= 3) {
+    print(j);
+    j++;
+  }
+
+
+
+  }
 
 void generatePattern(int rows) {
   String row = '';
@@ -98,7 +112,20 @@ void generatePattern(int rows) {
     // }
     print(row);
     row = '';
-    {
+    {}
+
+
+  int sum = 0;
+
+  for (int i = 1; i <= 20; i++) {
+    if (i % 2 == 0) {
+      sum += i;
     }
   }
+
+  print('The sum of all even numbers from 1 to 20 is: $sum');
 }
+
+
+}
+
