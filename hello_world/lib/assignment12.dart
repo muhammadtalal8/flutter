b598a6d5f0cb7052469260c2a10ab33ebdb2b9e1
muhammadtalal8 <muhammadtalalshakeel2009@gmail.com>
2024-08-19 17:25:13 +0500

    assignment12

--- hello_world/lib/assignment12.dart
+++ hello_world/lib/assignment12.dart
@@ -1,3 +1,53 @@
-void main() {
-  print('hello world');
+class Product {
+  final String name;
+  final double price;
+
+  Product(this.name, this.price);
 }
+
+class CartItem {
+  final Product product;
+  int quantity;
+
+  CartItem(this.product, this.quantity);
+}
+
+class ChangeNotifier {
+  List<CartItem> _items = [];
+
+  List<CartItem> get items => _items;
+
+  double get total {
+    return _items.fold(0.0, (sum, item) => sum + item.product.price * item.quantity);
+  }
+
+  void addToCart(Product product) {
+    final index = _items.indexWhere((item) => item.product.name == product.name);
+    if (index >= 0) {
+      _items[index].quantity += 1;
+    } else {
+      _items.add(CartItem(product, 1));
+    }
+    NotifyListeners();
+  }
+
+  void removeFromCart(Product product) {
+    final index = _items.indexWhere((item) => item.product.name == product.name);
+    if (index >= 0) {
+      if (_items[index].quantity > 1) {
+        _items[index].quantity -= 1;
+      } else {
+        _items.removeAt(index);
+      }
+      Notifylisteners();
+    }
+  }
+  
+  void Notifylisteners() {}
+}
+
+class NotifyListeners {
+}
+
+
+
