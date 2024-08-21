fd2d0529a3d780846b59da5082201d215ad8bfa0
muhammadtalal8 <muhammadtalalshakeel2009@gmail.com>
2024-08-21 21:42:37 +0500

    if

--- hello_world/lib/main1.dart
+++ hello_world/lib/main1.dart
@@ -1,23 +1,23 @@
 void main() {
   String destinationZone = 'PQR';
-  double weightinkg = 6;
+  double weightinkg = 8;
   if (destinationZone == 'PQR') {
-    print('shipping cost: ${weightinkg * 9}');
+    print('shipping cost: ${weightinkg * 7}');
   } else if (destinationZone == 'XYZ') {
     print('shipping cost: ${weightinkg * 8}');
   } else if (destinationZone == 'ABC') {
-    print('shipping cost: ${weightinkg * 7}');
+    print('shipping cost: ${weightinkg * 9}');
   } else {
     print('invalid destination zone');
   }
 
   switch (destinationZone) {
     case 'PQR':
-      print('shipping cost: ${weightinkg * 9}');
+      print('shipping cost: ${weightinkg * 8}');
       break;
     case 'XYZ':
       break;
-      print('shipping cost: ${weightinkg * 8}');
+      print('shipping cost: ${weightinkg * 9}');
 
     case 'ABC':
       break;
