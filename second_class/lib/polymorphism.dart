629a01242b5c36470379cfbe064dd0f27557d33b
muhammadtalal8 <muhammadtalalshakeel2009@gmail.com>
2024-08-25 18:13:04 +0500

    polymorphism

--- second_class/lib/polymorphism.dart
+++ second_class/lib/polymorphism.dart
@@ -1,10 +1,17 @@
-class Vehicle {
+class Vehicle1 {
   isRunning(String ab) {
     print("Vehicle is running");
   }
 }
 
-class Bike extends Vehicle {
+class Car1 extends Vehicle1 {
+  @override
+  isRunning(String ab) {
+    print("Car is running");
+  }
+}
+
+class Bike1 extends Vehicle1 {
   @override
   isRunning(String ab) {
     print("Bike is running");
