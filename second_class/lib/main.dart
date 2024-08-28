a829b5471637d911499a6d4486b19a2c0984e655
muhammadtalal8 <muhammadtalalshakeel2009@gmail.com>
2024-08-28 18:54:59 +0500

    first commit

--- second_class/lib/main.dart
+++ second_class/lib/main.dart
@@ -1,31 +1,10 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Row(
-    textDirection: TextDirection.ltr,
-    children: [
-      Expanded(child: Text(
-        'One',
-        textDirection: TextDirection.ltr,
-        style: TextStyle(fontSize: 40),
-      )),
-            Text(
-        'Two',
-        textDirection: TextDirection.ltr,
-        style: TextStyle(fontSize: 40),
-      ),
-            Text(
-        'Three',
-        textDirection: TextDirection.ltr,
-        style: TextStyle(fontSize: 40),
-      ),
-      
-            Text(
-        'Four',
-        textDirection: TextDirection.ltr,
-        style: TextStyle(fontSize: 40),
-      ),
-      
-    ],
+  runApp(Column(children: [
+    Text("1",textDirection: TextDirection.ltr,
+    style: TextStyle(fontSize: 40), 
+    )
+  ],
   ));
 }
