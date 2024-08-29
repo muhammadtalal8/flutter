f7bcb636e34a9733ad187b2e534dd321568372da
muhammadtalal8 <muhammadtalalshakeel2009@gmail.com>
2024-08-29 16:37:06 +0500

    first commit

--- second_class/lib/main.dart
+++ second_class/lib/main.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Column(children: [
-    Text("1",textDirection: TextDirection.ltr,
-    style: TextStyle(fontSize: 40), 
-    )
-  ],
-  ));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Name: Muhammad Talal'),
+              Text('Skills: Flutter, Dart, HTML, CSS'),
+              Text('Hobbies: Coding'),
+              Text('Profession: Software Developer'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
