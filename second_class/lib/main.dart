321832e5598f9a30b8885961e44c2758fd4feed0
muhammadtalal8 <muhammadtalalshakeel2009@gmail.com>
2024-08-30 14:16:16 +0500

    first commit

--- second_class/lib/main.dart
+++ second_class/lib/main.dart
@@ -1,38 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp(Container(
-    padding: EdgeInsets.symmetric(vertical: 60.0, horizontal: 100.0),
-// padding: EdgeInsets.all(10.0),
-decoration: BoxDecoration(color: Colors.blue),
+  runApp(Directionality(
+    
+    
+      textDirection: TextDirection.ltr,
+      
+      child: ListView(
+      
+        padding: EdgeInsets.all(100.0),
 
-    child: Column(
-      children: [
-        Expanded(child:Text("1",
-        textDirection: TextDirection.ltr,
-        style: TextStyle(fontSize: 100),
-        )),
-            Expanded(child:    Text("2",
-        textDirection: TextDirection.ltr,
-        style: TextStyle(fontSize: 100),
-        )),
-        Expanded(child:        Text("3",
-        textDirection: TextDirection.ltr,
-        style: TextStyle(fontSize: 100),
-        )),
-        Expanded(child:        Text("4",
-        textDirection: TextDirection.ltr,
-        style: TextStyle(fontSize: 100),
-        )),
-               Expanded(child: Text("5",
-        textDirection: TextDirection.ltr,
-        style: TextStyle(fontSize: 100),
-        )),
-                Expanded(child:Text("6",
-        textDirection: TextDirection.ltr,
-        style: TextStyle(fontSize: 100),
-        )),
-      ],
-    ),
-  ));
+        children: [
+          
+          Container(
+            height: 100,
+            color: Colors.deepPurple,
+              child: Center(
+            child: Text(
+              "1",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(fontSize: 100),
+            ),
+          )),
+                  Container(
+            height: 100,
+            color: Colors.deepOrange,
+              child: Center(
+            child: Text(
+              "2",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(fontSize: 100),
+            ),
+          )),
+          Container(
+            height: 100,
+            color: Colors.amberAccent,
+              child: Center(
+            child: Text(
+              "3",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(fontSize: 100),
+            ),
+          )),
+          Container(
+            height: 100,
+            color: Colors.blue,
+              child: Center(
+            child: Text(
+              "4",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(fontSize: 100),
+            ),
+          )),
+          Container(
+            height: 100,
+            color: Colors.blueGrey,
+              child: Center(
+            child: Text(
+              "5",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(fontSize: 100),
+            ),
+          )),
+        ],
+      )));
 }
