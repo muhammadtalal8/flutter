d65b1a898a79703de4e9caa03de8dc4e86d48957
muhammadtalal8 <muhammadtalalshakeel2009@gmail.com>
2024-08-30 13:33:22 +0500

    first commit

--- second_class/lib/main.dart
+++ second_class/lib/main.dart
@@ -1,26 +1,51 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(Column(
+    children: [
+      Expanded(child:
+      Text("1",
+      textDirection: TextDirection.ltr,
+      style: TextStyle(fontSize: 50),
+      )),
+        Expanded(child:   Text("2",
+      textDirection: TextDirection.ltr,
+      style: TextStyle(fontSize: 50),
+      )),
+      Expanded(child:      Text("3",
+      textDirection: TextDirection.ltr,
+      style: TextStyle(fontSize: 50),
+      )),
+       Expanded(child:     Text("4",
+      textDirection: TextDirection.ltr,
+      style: TextStyle(fontSize: 50),
+      )),
+             Expanded(child:     Text("5",
+      textDirection: TextDirection.ltr,
+      style: TextStyle(fontSize: 50),
+      )),
+             Expanded(child:     Text("6",
+      textDirection: TextDirection.ltr,
+      style: TextStyle(fontSize: 50),
+      )),
+      Expanded(child:     Text("7",
+      textDirection: TextDirection.ltr,
+      style: TextStyle(fontSize: 50),
+      )),
+      Expanded(child:     Text("8",
+      textDirection: TextDirection.ltr,
+      style: TextStyle(fontSize: 50),
+      )),
+      Expanded(child:     Text("9",
+      textDirection: TextDirection.ltr,
+      style: TextStyle(fontSize: 50),
+      )),
+      Expanded(child:     Text("10",
+      textDirection: TextDirection.ltr,
+      style: TextStyle(fontSize: 50),
+      )),
+    ],
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Name: Muhammad Talal'),
-              Text('Skills: Flutter, Dart, HTML, CSS'),
-              Text('Hobbies: Coding'),
-              Text('Profession: Software Developer'),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
+
