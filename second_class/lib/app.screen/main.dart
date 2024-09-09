071040cc6ac4f9194a71dce051aea9275d0fb11e
muhammadtalal8 <muhammadtalalshakeel2009@gmail.com>
2024-09-09 17:01:20 +0500

    first commit

--- second_class/lib/app.screen/main.dart
+++ second_class/lib/app.screen/main.dart
@@ -1,28 +1,72 @@
 import 'package:flutter/material.dart';
-import 'package:second_class/app.screen/home.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(LudoBoardApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-
-  
-
-  // This widget is the root of your application.
+class LudoBoardApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'WHATSAPP',
-      theme: ThemeData(
-        primaryColor:  const Color.fromARGB(255, 6, 92, 9), 
-        
-     
-      ),
+      home: LudoBoard(),
       debugShowCheckedModeBanner: false,
-      home: const MyHomePage(title: 'Whatsapp'),
+    );
+  }
+}
+
+class LudoBoard extends StatelessWidget {
+  // Number of rows/columns in the Ludo grid (15x15 for standard Ludo board)
+  final int boardSize = 15;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Ludo Board Design'),
+        backgroundColor: Colors.teal,
+      ),
+      body: Center(
+        child: Container(
+          width: 300,
+          height: 300,
+          color: Colors.black,
+          child: GridView.builder(
+            physics: NeverScrollableScrollPhysics(),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: boardSize,
+            ),
+            itemCount: boardSize * boardSize,
+            itemBuilder: (context, index) {
+              return LudoTile(index: index);
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class LudoTile extends StatelessWidget {
+  final int index;
+
+  LudoTile({required this.index});
+
+  @override
+  Widget build(BuildContext context) {
+    // Colors for the Ludo safe zones (top-left, top-right, bottom-left, bottom-right)
+    Color color = Colors.white;
+    if (index < 5 || index % 15 < 5) {
+      color = Colors.red[300]!;
+    } else if (index > 210 || index % 15 > 10) {
+      color = Colors.green[300]!;
+    } else if (index % 15 > 10 || (index % 15 < 5 && index > 100)) {
+      color = Colors.yellow[300]!;
+    } else if (index < 5 || (index % 15 < 5 && index > 100)) {
+      color = Colors.blue[300]!;
+    }
+    return Container(
+      margin: EdgeInsets.all(1),
+      color: color,
     );
   }
 }
