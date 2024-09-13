8c270fe8e2437a0aa75a4fffefdd56697797ce04
muhammadtalal8 <muhammadtalalshakeel2009@gmail.com>
2024-09-13 23:09:04 +0500

    first commit

--- second_class/lib/main.dart
+++ second_class/lib/main.dart
@@ -11,7 +11,7 @@ class LudoBoardApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: LudoBoard(),
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true,
     );
   }
 }
