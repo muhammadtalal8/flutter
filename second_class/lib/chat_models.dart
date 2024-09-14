27152028d7cd6fdba0fdacac6a18cba36bfc4e73
muhammadtalal8 <muhammadtalalshakeel2009@gmail.com>
2024-09-14 21:50:56 +0500

    first commit

---
+++ second_class/lib/chat_models.dart
@@ -0,0 +1,31 @@
+class ChatModel {
+  final String name;
+  final String message;
+  final String time;
+  final String avatar;
+  ChatModel({required this.name, required this.message, required this.time, required this.avatar});
+}
+
+List<ChatModel> chatData = [
+  ChatModel(
+name:"Talal",
+message: "Hello",
+time: "9:42",
+avatar: " ",
+  
+  ),
+  ChatModel(
+name:"Bilal",
+message: "Hi",
+time: "10:14",
+avatar: " ",
+  
+  ),
+  ChatModel(
+name:"khanjee",
+message: "bye",
+time: "10:34",
+avatar: "",
+  
+  ),
+];
